4e9650e1c25a228f2d9271ccbc54fd0ba33a3d31
issue <anshurock989@gmail.com>
2026-01-19 17:30:45 -0800

    issue03

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./app.scss"
+import Dock from './Components/Dock'
+
+const App = () => {
+  return (
+    <main>
+      <Dock/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Dock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dock = () => {
+  return (
+    <div>
+     <footer>
+        <div className="icon"><img src="/Doc-icons/github.png" alt="" /></div>
+        <div className="icon"><img src="/Doc-icons/notebook.pnggit" alt="" /></div>
+     </footer>
+    </div>
+  )
+}
+
+export default Dock
