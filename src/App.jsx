1ede80268e2ad758a2ac4f97c85694f98880fd4f
Anshu <anshurock989@gmail.com>
2026-07-15 22:20:54 +0530

    terminal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import "./app.scss"
 import Dock from './Components/Dock'
 import Navbar from './Components/Navbar'
+import MacWindows from './Components/Windows/MacWindows'
 
 const App = () => {
   return (
     <main>
       <Dock/>
       <Navbar/>
+      <MacWindows>
+        <h1>Hello</h1>
+      </MacWindows>
     </main>
   )
 }
